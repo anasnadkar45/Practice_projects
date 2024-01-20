c0805ef4733d0aefb1c12cc8d3fb2a72de7021fe
anasnadkar45 <anasnadkar23@gmail.com>
2024-01-20 10:41:44 +0530

    updating Redux todo Project

--- 02_Redux_Todo/src/components/AddTodo.jsx
+++ 02_Redux_Todo/src/components/AddTodo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { addTodo} from '../Features/Todo/todoSlice'
-import {  useDispatch } from 'react-redux'
+import { addTodo } from '../Features/Todo/todoSlice'
+import { useDispatch } from 'react-redux'
 
 function AddTodo() {
     const [input, setInput] = useState('');
@@ -14,16 +14,21 @@ function AddTodo() {
     }
 
     return (
-        <div>
-            <form action="" onSubmit={todoHandler}>
-                <input
-                    type="text"
-                    placeholder='Enter Todo'
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)} />
-                <button type='submit'>Add</button>
-            </form>
-        </div>
+        <form onSubmit={todoHandler} className="space-x-3 mt-12">
+            <input
+                type="text"
+                className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Enter a Todo..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <button
+                type="submit"
+                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+                Add Todo
+            </button>
+        </form>
     )
 }
 
